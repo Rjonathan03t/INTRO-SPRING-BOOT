ea64578926b52b48ff45905378d0ae93fc1d28ec
jonathan rajao <rjonathan2003t@gmail.com>
2023-08-20 13:36:08 +0300

    feat: add project

---
+++ Introduction_Spring_Boot-STD22105/src/main/java/introduction_spring_bootstd22105/IntroductionSpringBootStd22105Application.java
@@ -0,0 +1,12 @@
+package introduction_spring_bootstd22105;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class IntroductionSpringBootStd22105Application {
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionSpringBootStd22105Application.class, args);
+	}
+}
---
+++ Introduction_Spring_Boot-STD22105/src/test/java/introduction_spring_bootstd22105/IntroductionSpringBootStd22105ApplicationTests.java
@@ -0,0 +1,13 @@
+package introduction_spring_bootstd22105;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionSpringBootStd22105ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
